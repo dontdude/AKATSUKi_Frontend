f2f6660fda10cf53f5bbf603ac1c81757eaf7fa6
Chandan Mishra <imchandan1947@gmail.com>
2022-11-09 03:38:31 +0530

    Suggestions added

--- components/MiniProfile.js
+++ components/MiniProfile.js
@@ -2,19 +2,19 @@ import React from 'react'
 
 const MiniProfile = () => {
   return (
-    <div className='flex items-center justify-between mt-14 ml-10'>
+    <div className='flex items-center justify-between mt-12 mx-8'>
       <img
-        className='w-16 h-16 rounded-full border p-[2px]'
+        className='w-16 h-16 rounded-full border p-1'
         src='https://portfolio-dontdude.vercel.app/static/media/logo.f202a4f66203c8577fc1.jpg'
         alt='User profile picture'
       />
       
-      <div>
-        <h2>mchandan</h2>
-        <h3>Welcome to AKATSUKi Anime Community</h3>
+      <div className='flex-1 mx-4'>
+        <h2 className='font-bold'>mchandan</h2>
+        <h3 className='text-sm text-gray-500'>Chandan Mishra <br/> Welcome to AKATSUKi</h3>
       </div>
 
-      <button>Sign Out</button>
+      <button className='font-semibold text-blue-500'>Sign Out</button>
     </div>
   )
 }
--- components/Suggestions.js
+++ components/Suggestions.js
@@ -1,9 +1,67 @@
-import React from 'react'
+import { useEffect, useState } from "react";
 
-const Suggestions = () => {
+const sugg = [
+  {
+    id: 1,
+    avatar:
+      "https://m.cricbuzz.com/a/img/v1/192x192/i1/c170661/virat-kohli.jpg",
+    username: "virat kohli",
+    company: {
+      name: "BCC",
+    },
+  },
+  {
+    id: 2,
+    avatar:
+      "https://upload.wikimedia.org/wikipedia/commons/9/98/Tom_Cruise_%2834450932580%29.jpg",
+    username: "Tom cruise",
+    company: {
+      name: "Hollywood",
+    },
+  },
+  {
+    id: 3,
+    avatar:
+      "https://upload.wikimedia.org/wikipedia/commons/0/0c/Angelina_Jolie_by_Gage_Skidmore.jpg",
+    username: "Angelina jolie",
+    company: {
+      name: "Hollywood",
+    },
+  },
+];
+
+function Suggestions() {
+  const [suggestions, setSuggestions] = useState([]);
+  useEffect(() => {
+    setSuggestions(sugg);
+  }, []);
   return (
-    <div>Suggestions</div>
-  )
+    <div className="mt-4 ml-10">
+      <div className="flex justify-between text-sm mb-5">
+        <h3 className="text-sm font-bold text-gray-400">Suggestions for you</h3>
+        <button className="text-gray-600 font-semibold">See All</button>
+      </div>
+      {suggestions.map((profile) => (
+        <div
+          key={profile.id}
+          className="flex items-center justify-between mt-3"
+        >
+          <img
+            className="rounded-full w-10 h-10 object-cover border p-[2px]"
+            src={profile.avatar}
+            alt=""
+          />
+          <div className="flex-1 ml-4">
+            <h2 className="text-sm font-semibold">{profile.username}</h2>
+            <h3 className="text-sm text-gray-400">
+              Works at {profile.company.name}
+            </h3>
+          </div>
+          <button className="text-blue-500">Follow</button>
+        </div>
+      ))}
+    </div>
+  );
 }
 
-export default Suggestions
+export default Suggestions;
